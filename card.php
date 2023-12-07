dd93e5a26e1f98289a77196201f3e7d740e48e82
wisdom26 <42862067+wisdom26@users.noreply.github.com>
2023-12-07 12:19:09 +0530

    Update card.php

--- card.php
+++ card.php
@@ -3,23 +3,23 @@
 <div class="card ">
   <img src="img_avatar.png" alt="Avatar" style="width:100%">
   <div class="container">
-    <h4><b>John Doe</b></h4> 
-    <p>Architect & Engineer</p> 
+    <h4><b>Pooja Patel</b></h4> 
+    <p></p> 
   </div>
 </div><br><br>
 <div class="card">
 <i class="bi bi-person-bounding-box"></i>
     <img src="img_avatar.png" alt="Avatar" style="width:100%">
     <div class="container">
-      <h4><b>John Doe</b></h4> 
-      <p>Architect & Engineer</p> 
+      <h4><b>Vishmitha M H </b></h4> 
+      <p></p> 
     </div>
   </div><br><br>
   <div class="card">
     <img src="img_avatar.png" alt="Avatar" style="width:100%">
     <div class="container">
-      <h4><b>John Doe</b></h4> 
-      <p>Architect & Engineer</p> 
+      <h4><b>Vivek Kumar Maurya</b></h4> 
+      <p></p> 
     </div>
   </div>
 </div>
